254f2f65e3f054924d69ecd0d72190c86e83bc1a
Mijail Rondon <mijail@hey.com>
2020-07-07 22:05:29 -0500

    Add console command

--- terraform.rb
+++ terraform.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 cheatsheet do
-  title 'Terraform'
-  docset_file_name 'Terraform'
-  keyword 'terraform'
+  title "Terraform"
+  docset_file_name "Terraform"
+  keyword "terraform"
 
   introduction <<-'INTRO'
   Terraform is a tool for building, changing, and versioning infrastructure
@@ -14,7 +14,7 @@ cheatsheet do
   INTRO
 
   category do
-    id 'apply'
+    id "apply"
 
     entry do
       notes <<-'NOTES'
@@ -31,12 +31,12 @@ cheatsheet do
     end
 
     entry do
-      name '-auto-approve'
-      notes 'Skip interactive approval of plan before applying.'
+      name "-auto-approve"
+      notes "Skip interactive approval of plan before applying."
     end
 
     entry do
-      name '-backup=path'
+      name "-backup=path"
       notes <<-'NOTES'
       Path to backup the existing state file before modifying.
       Defaults to the `-state-out` path with `.backup` extension.
@@ -45,7 +45,7 @@ cheatsheet do
     end
 
     entry do
-      name '-compact-warnings'
+      name "-compact-warnings"
       notes <<-'NOTES'
       If Terraform produces any warnings that are not
       accompanied by errors, show them in a more compact
@@ -54,32 +54,32 @@ cheatsheet do
     end
 
     entry do
-      name '-lock=true'
-      notes 'Lock the state file when locking is supported.'
+      name "-lock=true"
+      notes "Lock the state file when locking is supported."
     end
 
     entry do
-      name '-lock-timeout=0s'
-      notes 'Duration to retry a state lock.'
+      name "-lock-timeout=0s"
+      notes "Duration to retry a state lock."
     end
 
     entry do
-      name '-input=true'
-      notes 'Ask for input for variables if not directly set.'
+      name "-input=true"
+      notes "Ask for input for variables if not directly set."
     end
 
     entry do
-      name '-no-color'
+      name "-no-color"
       notes 'If specified, output won\'t contain any color.'
     end
 
     entry do
-      name '-parallelism=n'
-      notes 'Limit the number of parallel resource operations.'
+      name "-parallelism=n"
+      notes "Limit the number of parallel resource operations."
     end
 
     entry do
-      name '-refresh=true'
+      name "-refresh=true"
       notes <<-'NOTES'
       Update state prior to checking for differences. This
       has no effect if a plan file is given to apply.
@@ -87,7 +87,7 @@ cheatsheet do
     end
 
     entry do
-      name '-state=path'
+      name "-state=path"
       notes <<-'NOTES'
       Path to read and save state (unless state-out
       is specified). Defaults to `terraform.tfstate`
@@ -95,7 +95,7 @@ cheatsheet do
     end
 
     entry do
-      name '-state-out=path'
+      name "-state-out=path"
       notes <<-'NOTES'
       Path to write state to that is different than
       `-state`. This can be used to preserve the old
@@ -104,7 +104,7 @@ cheatsheet do
     end
 
     entry do
-      name '-target=resource'
+      name "-target=resource"
       notes <<-'NOTES'
       Resource to target. Operation will be limited to this
       resource and its dependencies. This flag can be used
@@ -121,12 +121,57 @@ cheatsheet do
     end
 
     entry do
-      name '-var-file=foo'
+      name "-var-file=foo"
       notes <<-'NOTES'
       Set variables in the Terraform configuration from
       a file. If "terraform.tfvars" or any `.auto.tfvars`
       files are present, they will be automatically loaded.
       NOTES
+    end
+  end
+
+  category do
+    id "console"
+
+    entry do
+      notes <<-'NOTES'
+        __Starts an interactive console for experimenting with Terraform
+        interpolations.__
+
+        This will open an interactive console that you can use to type
+        interpolations into and inspect their values. This command loads the
+        current state. This lets you explore and test interpolations before
+        using them in future configurations.
+
+        _This command will never modify your state._
+
+        `DIR` can be set to a directory with a Terraform state to load. By
+        default, this will default to the current working directory.
+        NOTES
+    end
+
+    entry do
+      name "-state=path"
+      notes <<-'NOTES'
+        Path to read state. Defaults to `terraform.tfstate`
+        NOTES
+    end
+
+    entry do
+      name '-var \'foo=bar\''
+      notes <<-'NOTES'
+        Set a variable in the Terraform configuration. This
+        flag can be set multiple times.
+        NOTES
+    end
+
+    entry do
+      name "-var-file=foo"
+      notes <<-'NOTES'
+        Set variables in the Terraform configuration from
+        a file. If "terraform.tfvars" or any `.auto.tfvars`
+        files are present, they will be automatically loaded.
+        NOTES
     end
   end
 end
