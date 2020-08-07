98145d2e44987fdb9f41f2897b87694e6687c812
Mijail Rondon <mijail@hey.com>
2020-08-06 21:38:31 -0500

    Add command validate

--- terraform.rb
+++ terraform.rb
@@ -1198,4 +1198,53 @@ cheatsheet do
       NOTES
     end
   end
+
+  category do
+    id "validate"
+
+    entry do
+      notes <<-'NOTES'
+      Usage: terraform validate [options] [dir]
+
+      Validate the configuration files in a directory, referring only to the
+      configuration and not accessing any remote services such as remote state,
+      provider APIs, etc.
+      
+      Validate runs checks that verify whether a configuration is syntactically
+      valid and internally consistent, regardless of any provided variables or
+      existing state. It is thus primarily useful for general verification of
+      reusable modules, including correctness of attribute names and value types.
+      
+      It is safe to run this command automatically, for example as a post-save
+      check in a text editor or as a test step for a re-usable module in a CI
+      system.
+      
+      Validation requires an initialized working directory with any referenced
+      plugins and modules installed. To initialize a working directory for
+      validation without accessing any configured remote backend, use:
+          terraform init -backend=false
+      
+      If dir is not specified, then the current directory will be used.
+      
+      To verify configuration in the context of a particular run (a particular
+      target workspace, input variable values, etc), use the 'terraform plan'
+      command instead, which includes an implied validation check.
+      NOTES
+    end
+
+    entry do
+      name "-json"
+      notes <<-'NOTES'
+      Produce output in a machine-readable JSON format, suitable for
+      Always disables color.
+      NOTES
+    end
+
+    entry do
+      name "-no-color"
+      notes <<-'NOTES'
+      If specified, output won't contain any color.
+      NOTES
+    end
+  end
 end
